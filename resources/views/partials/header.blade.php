bc6acf50791d41909502b2a9964b7d61216d3210
Elena <elena.r@vivifyideas.com>
2018-11-02 15:29:06 +0100

    #LVAJ-9 Added layout

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,43 @@
+   <a class="navbar-brand" href="{{ url('/') }}">
+    {{ config('app.name', 'Laravel') }}
+</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <!-- Left Side Of Navbar -->
+    <ul class="navbar-nav mr-auto">
+
+    </ul>
+
+    <!-- Right Side Of Navbar -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Authentication Links -->
+        @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            <li class="nav-item">
+                @if (Route::has('register'))
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                @endif
+            </li>
+        @else
+            <li class="nav-item">
+                {{ Auth::user()->first_name }} <span></span>                
+                {{ Auth::user()->last_name }} <span></span> 
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+            </div>
+        @endguest
+    </ul>
+</div>
+
